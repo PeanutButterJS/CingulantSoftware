3720e309da95bc39abca0b7de8dad8e38ccf5103
PeanutButterJS <PntBtrJS@gmail.com>
2018-08-24 11:16:09 +0300

    formatstyled

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,27 +1,64 @@
 import Styled from 'styled-components';
-import PropTypes from 'prop-types';
 import Logo from '../logo';
 
-export const Hero = ({ className }) => (
-  <div className={className}>
-    <div className="inner">
-      <div className="logo">
-        <Logo />
-        <h2 className="firstText">Cingulant Software</h2>
-        <p className="secondText">
-          Outsource the right way - the Cingulant way!
-        </p>
-      </div>
-    </div>
-  </div>
-);
+const Inner = Styled.div`
+  margin: 0 auto;
+  width: 55em;
 
-Hero.propTypes = {
-  className: PropTypes.string
-};
+  @media screen and (max-width: 980px) {
+    width: 100%;
+  }
+`;
 
-export default Styled(Hero)`
-  color: white;
+const LogoWrapper = Styled.div`
+ transition: opacity 2s ease, transform 1s ease;
+    transform: translateY(0);
+    opacity: 1; 
+    margin: 0 0 1.3em 0;
+    transition: all 0.5s ease;
+
+    @media screen and (max-width: 736px) {
+      margin: 0 0 1em 0;
+    }
+`;
+
+const TitleMainText = Styled.h2`
+    transition: opacity 0.5s ease, transform 0.5s ease, filter 0.25s ease;
+    transform: translateX(0);
+    transition-delay: 0.65s;
+    filter: blur(0);
+    opacity: 1;
+    border-bottom: solid 2px rgba(255, 255, 255, 0.125);
+    font-size: 2.25em;
+    margin-bottom: 0.6em;
+    padding-bottom: 0.4em;
+    letter-spacing: 0.1em;
+    text-transform: uppercase;   
+
+    @media screen and (max-width: 736px) {
+      font-size: 1.5em;
+    }
+`;
+
+const TitleSecondText = Styled.p`
+transition: opacity 0.5s ease, transform 0.5s ease, filter 0.25s ease;
+    transform: translateX(0);
+    transition-delay: 0.8s;
+    filter: blur(0);
+    opacity: 1;
+    font-size: 1em;
+    letter-spacing: 0.1em;
+    line-height: 2;
+    text-transform: uppercase; 
+    font-family: 'OpenSansLight'; 
+
+    @media screen and (max-width: 736px){
+    font-size: 0.8em;
+    }
+`;
+
+const StyledHero = Styled.div`
+ color: white;
   padding: 10em 0 4.75em 0;
 
   @media screen and (max-width: 1280px) {
@@ -38,60 +75,20 @@ export default Styled(Hero)`
     padding: 5em 2em 4.25em 2em;
     margin-bottom: -2.5em;
   }
-
-  .inner {
-    margin: 0 auto;
-    width: 55em;
-
-    @media screen and (max-width: 980px) {
-      width:100%;
-    }
-  }
-
-  .logo {
-    transition: opacity 2s ease, transform 1s ease;
-    transform: translateY(0);
-    opacity: 1; 
-    margin: 0 0 1.3em 0;
-    transition: all 0.5s ease;
-
-    @media screen and (max-width: 736px) {
-      margin: 0 0 1em 0;
-    }
-  }
-
-  .firstText {
-    transition: opacity 0.5s ease, transform 0.5s ease, filter 0.25s ease;
-    transform: translateX(0);
-    transition-delay: 0.65s;
-    filter: blur(0);
-    opacity: 1;
-    border-bottom: solid 2px rgba(255, 255, 255, 0.125);
-    font-size: 2.25em;
-    margin-bottom: 0.6em;
-    padding-bottom: 0.4em;
-    letter-spacing: 0.1em;
-    text-transform: uppercase;   
-
-    @media screen and (max-width: 736px) {
-      font-size: 1.5em;
-    }
-  }
-
-  .secondText {
-    transition: opacity 0.5s ease, transform 0.5s ease, filter 0.25s ease;
-    transform: translateX(0);
-    transition-delay: 0.8s;
-    filter: blur(0);
-    opacity: 1;
-    font-size: 1em;
-    letter-spacing: 0.1em;
-    line-height: 2;
-    text-transform: uppercase; 
-    font-family: 'OpenSansLight'; 
-
-    @media screen and (max-width: 736px){
-    font-size: 0.8em;
-    }
-  }
 `;
+
+export const Hero = () => (
+  <StyledHero>
+    <Inner>
+      <LogoWrapper>
+        <Logo />
+        <TitleMainText>Cingulant Software</TitleMainText>
+        <TitleSecondText>
+          Outsource the right way - the Cingulant way!
+        </TitleSecondText>
+      </LogoWrapper>
+    </Inner>
+  </StyledHero>
+);
+
+export default Hero;
