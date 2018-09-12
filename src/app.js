b9de105f900e3ef15eabf8edef48bf6563ca9da7
PeanutButterJS <PntBtrJS@gmail.com>
2018-09-12 14:20:23 +0300

    delete

--- src/app.js
+++ src/app.js
@@ -1,17 +1,8 @@
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import Hero from './components/hero';
 import MainContent from './components/main-content';
-// import PageContent from './components/page-content';
 import Footer from './components/footer';
 import PageWrapper from './components/page-wrapper';
 import Header from './components/header';
-/* import Descriptions from './components/descriptions';
-import BottomContent from './components/bottom-content';
-import Testimonials from './components/testimonials';
-import Gallery from './components/gallery';
-import TransparentContent from './components/transparent-content'; */
-// import Intro from './components/intro';
 
 const App = () => (
   <PageWrapper>
@@ -19,13 +10,6 @@ const App = () => (
     <Hero />
     <MainContent />
     <Footer />
-
-    {/*      <Descriptions />
-    <TransparentContent />
-    <Gallery />
-    <Testimonials />
-    <BottomContent />
-     */}
   </PageWrapper>
 );
 
