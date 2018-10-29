f02499e07eb11a10b8fc17d3e51e6e20b4842950
PeanutButterJS <PntBtrJS@gmail.com>
2018-10-29 15:01:51 +0200

    d

--- src/components/common/contact-link/index.js
+++ src/components/common/contact-link/index.js
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types';
 
 const Link = styled.a`
   color: ${props => props.theme.darkBlue};
-  padding: 20px 40px;
+  padding: ${props => (props.isHeader ? '10px 15px' : '20px 40px')};
   font-size: 0.8em;
   z-index: 1;
   display: inline-block;
   position: relative;
   font-weight: bold;
   text-decoration: none;
-  border-radius: 9px;
+  border-radius: ${props => (props.isHeader ? '4px' : '9px')};
   box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.2);
   background: ${props =>
     `linear-gradient(${lighten(0.15, props.theme.orange)}, ${
